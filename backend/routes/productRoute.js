b3d166ac8aa94ea49540b93fac384ee54ad4bb95
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-10 14:39:51 +0530

    Set "product/:id" route using put method

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { AddNewProduct, getAllProducts } = require('../controllers/productController');
+const { AddNewProduct, getAllProducts, updateProduct } = require('../controllers/productController');
 
 router.route('/new').post(AddNewProduct);
 router.route('/products').get(getAllProducts);
+router.route('/:id').put(updateProduct)
 
 module.exports = router;

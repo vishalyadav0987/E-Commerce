6a0e7b2ecbd618d19379d5ffaf44ea3311a7cc0d
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-10 14:41:32 +0530

    Create the logic update the product taking there id from the params from database using findByIdAndUpdate method

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -33,7 +33,37 @@ const getAllProducts = async (req, res) => {
         });
     }
 }
+
+// ADMIN --- Controller --- Admin Rights
+const updateProduct = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const productExsist = await ProductSchema.findById(id);
+        if(!productExsist){
+            return res.status(400).json({
+                success: false,
+                message: "Product not found!",
+            });
+        }
+        const product = await ProductSchema.findByIdAndUpdate(id,req.body,{
+            new:true,
+            runValidators:true,
+        });
+        res.status(200).json({
+            success: true,
+            message:"Product is succesfully updated!"
+        });
+    } catch (error) {
+        console.log("Error in updateProduct function: ", error.message);
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong, Product is not updated",
+            error: error.message
+        });
+    }
+}
 module.exports = {
     AddNewProduct,
-    getAllProducts
+    getAllProducts,
+    updateProduct
 }

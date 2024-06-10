ea888a9278740c83abcb3666e55ae63a0bc8cb75
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-10 18:12:10 +0530

    Add logic handle the uncaught and unhandledRejection error in code or server

--- backend/index.js
+++ backend/index.js
@@ -5,24 +5,43 @@ const PORT = 3000 || process.env.PORT;
 const connectDB = require('./connectDB/connect');
 const productRoutes = require('./routes/productRoute')
 
+
+// Handled uncaught exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server rejection due to uncaughtException`);
+    server.close(() => {
+        process.exit(1);
+    })
+});
+
 app.use(express.json());
 
 
-app.use('/api/v1/product',productRoutes);
+app.use('/api/v1/product', productRoutes);
 
 // TEST ENDPOINT
-app.get('/test',(req,res)=>{
+app.get('/test', (req, res) => {
     res.send("This is E-Kart website.");
 });
 
-const start = async()=>{
+const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
-        app.listen(PORT,()=>{
+        const server = app.listen(PORT, () => {
             console.log(`Server running at http://localhost:${PORT}`);
         })
+        // Unhandled promises rejection
+        process.on("unhandledRejection", (err) => {
+            console.log(`Error: ${err.message}`);
+            console.log(`Shutting down the server rejection due to unhandled Promise rejection`);
+            server.close(() => {
+                process.exit(1);
+            })
+        });
     } catch (error) {
-        console.log("Something went wrong, Please check the Database");        
+        console.log("Something went wrong, Please check the Database");
+        process.exit(1);  // Ensure process exits if DB connection fails
     }
 }
 

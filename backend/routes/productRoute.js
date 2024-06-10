21e203173180eff84d118239c0a04c1dc5e039b7
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-10 17:47:46 +0530

    Set "product/:id" route using delete method

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { AddNewProduct, getAllProducts, updateProduct } = require('../controllers/productController');
+const { AddNewProduct,
+    getAllProducts,
+    updateProduct,
+    removeProduct } = require('../controllers/productController');
 
 router.route('/new').post(AddNewProduct);
 router.route('/products').get(getAllProducts);
-router.route('/:id').put(updateProduct)
+router.route('/:id').put(updateProduct).delete(removeProduct);
 
 module.exports = router;

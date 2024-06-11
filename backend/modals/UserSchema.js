887872ad6b5df37d670bb9387ecde7e17f0d2e43
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-11 09:51:18 +0530

    Create a function hashPassword. If I update a user, the password should not be hashed again. That's why I use the isModified method.

--- backend/modals/UserSchema.js
+++ backend/modals/UserSchema.js
@@ -1,43 +1,52 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs')
+
 
 const UserSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,"Please enter your name"],
-        maxLength:[30,"Name cannot exceed 30 character"],
-        minLength:[4,"Name Should have more than 4 character"],
+    name: {
+        type: String,
+        required: [true, "Please enter your name"],
+        maxLength: [30, "Name cannot exceed 30 character"],
+        minLength: [4, "Name Should have more than 4 character"],
     },
-    email:{
-        type:String,
-        required:[true,"Please Enter your email"],
-        unique:true,
-        validate:[validator.isEmail,"Please Enter valid email"],
+    email: {
+        type: String,
+        required: [true, "Please Enter your email"],
+        unique: true,
+        validate: [validator.isEmail, "Please Enter valid email"],
     },
-    password:{
-        type:String,
-        required:[true,"Please Enter your password"],
-        minLength:[8,"Password should greater than 8 character"],
-        select:false,
+    password: {
+        type: String,
+        required: [true, "Please Enter your password"],
+        minLength: [8, "Password should greater than 8 character"],
+        select: false,
     },
-    avatar:{
-        public_id:{
-            type:String,
-            required:true,
+    avatar: {
+        public_id: {
+            type: String,
+            required: true,
         },
-        url:{
-            type:String,
-            required:true,
+        url: {
+            type: String,
+            required: true,
         }
     },
-    role:{
-        type:String,
-        default:"user"
+    role: {
+        type: String,
+        default: "user"
     },
-    resetPasswordToken:String,
-    resetPasswordDate:Date,
+    resetPasswordToken: String,
+    resetPasswordDate: Date,
 });
 
 
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified()) { // This cond check if password phle se hash toh dubara hash nh karega
+        next();
+    }
+    this.password = bcrypt.hash(this.password, 10);
+});
 
-module.exports=mongoose.models.user || mongoose.model("user",UserSchema);
+
+module.exports = mongoose.models.user || mongoose.model("user", UserSchema);

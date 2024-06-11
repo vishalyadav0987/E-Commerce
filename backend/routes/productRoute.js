15ccb45b3461243826cf191738e7b5ca1796dd25
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-11 15:41:09 +0530

    Set "product/:id" route using get method

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const { AddNewProduct,
     getAllProducts,
     updateProduct,
-    removeProduct } = require('../controllers/productController');
+    removeProduct,
+    getSingleProduct } = require('../controllers/productController');
 
 router.route('/new').post(AddNewProduct);
 router.route('/products').get(getAllProducts);
-router.route('/:id').put(updateProduct).delete(removeProduct);
+router.route('/:id').put(updateProduct).delete(removeProduct).get(getSingleProduct);
 
 module.exports = router;

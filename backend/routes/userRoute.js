4f6988844f0a0061207f5f8f6a2091a5c728ee00
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-13 15:04:11 +0530

    Set "user/me" route using get method pass the protectectedroute function

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { Register, Login, Logout, forgetPassword, resetPassword } = require('../controllers/userController')
+const { Register,
+    Login,
+    Logout,
+    forgetPassword,
+    resetPassword,
+    getUserOwnDetails
+} = require('../controllers/userController');
+const { protecteRoute } = require('../middleware/protectedRoute')
 
 
 router.route('/register').post(Register);
 router.route('/login').post(Login);
 router.route('/password/forget').post(forgetPassword);
 router.route('/password/reset/:token').put(resetPassword);
+router.route('/me').get(protecteRoute,getUserOwnDetails);
 router.route('/logout').get(Logout);
 
 module.exports = router;

5abacb6e1ce68702daf020f3fbddca4265a36d18
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-13 14:47:44 +0530

    set "/password/forget" froget password route and set "/password/reset/:token" using post and put  method respectively.

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { Register, Login, Logout } = require('../controllers/userController')
+const { Register, Login, Logout, forgetPassword, resetPassword } = require('../controllers/userController')
 
 
 router.route('/register').post(Register);
 router.route('/login').post(Login);
+router.route('/password/forget').post(forgetPassword);
+router.route('/password/reset/:token').put(resetPassword);
 router.route('/logout').get(Logout);
 
 module.exports = router;

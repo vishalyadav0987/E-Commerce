bf55493ae2b55af7ac5e4946ecb2f46d3add07cc
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-14 21:30:36 +0530

    Set "order/:id" route using get method for getSingleOrderDetail

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {protecteRoute,authorizeRole} = require('../middleware/protectedRoute');
-const { newOrder } = require('../controllers/orderController');
+const { protecteRoute, authorizeRole } = require('../middleware/protectedRoute');
+const {
+    newOrder,
+    getSingleOrderDetail,
+} = require('../controllers/orderController');
 
-router.route('/place').post(protecteRoute,newOrder);
+router.route('/place').post(protecteRoute, newOrder);
+router.route('/:id').get(protecteRoute, getSingleOrderDetail);
 
 module.exports = router;

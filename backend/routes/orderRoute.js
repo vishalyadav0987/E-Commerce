94fbab967f4370aaaaff292fc189b26ea0499182
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-14 23:24:17 +0530

    Set "/order/orders" using get method fetch all order for admin panel add authorizeRole("admin")

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -5,10 +5,12 @@ const {
     newOrder,
     getSingleOrderDetail,
     myOrder,
+    getAllOrders
 } = require('../controllers/orderController');
 
 router.route('/orders/place').post(protecteRoute, newOrder);
 router.route('/orders/:id').get(protecteRoute, getSingleOrderDetail);
-router.route('/me/orders').get(protecteRoute,myOrder);
+router.route('/me/orders').get(protecteRoute, myOrder);
+router.route('/orders').get(protecteRoute, authorizeRole("admin"), getAllOrders);
 
 module.exports = router;

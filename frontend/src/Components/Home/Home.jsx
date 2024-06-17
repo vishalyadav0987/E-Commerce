51f4dcbb783cecedff88f233d275d082d712caa3
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-17 16:39:33 +0530

    Create the home component and add two more component in Hero section and Products list

---
+++ frontend/src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../Hero/Hero';
+import ProductDisplay from '../ProductDispaly/ProductDisplay';
+
+const Home = () => {
+    return (
+        <div>
+            <Hero />
+            <ProductDisplay />
+        </div>
+    )
+}
+
+export default Home

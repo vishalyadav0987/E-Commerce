4eb5cac2e814973f1f81c2989e3b0a1f3a8991b2
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-17 16:40:22 +0530

    Set the home route ['/']

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Navbar from './Components/Navbar/Navbar';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import webFont from "webfontloader"
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+
 
 const App = () => {
+
+  useEffect(() => {
+    webFont.load({
+      google: {
+        families: ["Roboto", "Droid sans", "Chilanka"]
+      }
+    })
+  }, [])
   return (
     <div>
-      <h1>This is E-kart Project</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   )
 }

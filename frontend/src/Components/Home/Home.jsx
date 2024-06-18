844d78016cb70b25cc98a35422f3a031f6fd7548
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-18 09:18:51 +0530

    Call getAllProduct function taking productAction file and use the useDispatch and useSelector from react redux useSelctor helps taking data from the state and useDispatch dispatch data and also use the useEffect for dispatching the data from one component to another component and pass all the valus of {loading,error,product} taking from  state pass in ProductDisplay components

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Hero from '../Hero/Hero';
 import ProductDisplay from '../ProductDispaly/ProductDisplay';
+import { getAllProducts } from '../../actions/productAction';
+import { useSelector, useDispatch } from 'react-redux'
 
 const Home = () => {
+    const dispatch = useDispatch();
+    const {loading,error,product} = useSelector(state=>state.products)
+
+
+    useEffect(() => {
+        dispatch(getAllProducts())
+    }, [dispatch]);
+
     return (
         <div>
             <Hero />
-            <ProductDisplay />
+            <ProductDisplay loading={loading} error={error} product={product}/>
         </div>
     )
 }

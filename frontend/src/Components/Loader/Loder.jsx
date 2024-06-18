8c9f21543d9012f6b20e87b0639e531f1af91154
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-18 08:58:31 +0530

    Create the Design loading bar

---
+++ frontend/src/Components/Loader/Loder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Loader.css'
+
+const Loder = () => {
+  return (
+    <div className='Loader'>
+      <div className="spinner"></div>
+    </div>
+  )
+}
+
+export default Loder

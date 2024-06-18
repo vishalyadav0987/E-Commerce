9de4d55474ccf466e6894a688b4391d75bc1dcf2
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-18 15:15:59 +0530

    Create the getSingleProduct details fetch using get request using axios library ["/api/v1/product/${id}"]

--- frontend/src/actions/productAction.jsx
+++ frontend/src/actions/productAction.jsx
@@ -3,6 +3,9 @@ import {
     ALL_PRODUCT_REQUEST,
     ALL_PRODUCT_SUCEESS,
     ALL_PRODUCT_FAIL,
+    SINGLE_PRODUCT_REQUEST,
+    SINGLE_PRODUCT_SUCEESS,
+    SINGLE_PRODUCT_FAIL,
     CLEAR_ERRORS,
 } from '../constants/productConstants';
 
@@ -17,6 +20,23 @@ const getAllProducts = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: ALL_PRODUCT_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+//Get Single product detail
+const getSingleProducts = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: SINGLE_PRODUCT_REQUEST });
+        const response = await axios.get(`/api/v1/product/${id}`);
+        dispatch({
+            type: SINGLE_PRODUCT_SUCEESS,
+            payload: response.data.data,
+        })
+    } catch (error) {
+        dispatch({
+            type: SINGLE_PRODUCT_FAIL,
             payload: error.response.data.message
         })
     }

3c602bfe54c11ba9e49259008c43c7bbd1b6ae98
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-18 15:14:27 +0530

    Fetch data using product id as params from database store in state name is product in object

--- frontend/src/reducers/productReducer.jsx
+++ frontend/src/reducers/productReducer.jsx
@@ -2,6 +2,9 @@ import {
     ALL_PRODUCT_REQUEST,
     ALL_PRODUCT_SUCEESS,
     ALL_PRODUCT_FAIL,
+    SINGLE_PRODUCT_REQUEST,
+    SINGLE_PRODUCT_SUCEESS,
+    SINGLE_PRODUCT_FAIL,
     CLEAR_ERRORS,
 } from '../constants/productConstants';
 const productReducer = (state = { products: [] }, action) => {
@@ -32,4 +35,32 @@ const productReducer = (state = { products: [] }, action) => {
     }
 }
 
-export default productReducer;
+
+const singleProductReducer = (state = { product: {} }, action) => {
+    switch (action.type) {
+        case SINGLE_PRODUCT_REQUEST:
+            return {
+                loading: true,
+                ...state
+            }
+        case SINGLE_PRODUCT_SUCEESS:
+            return {
+                loading: false,
+                product: action.payload,
+            }
+        case SINGLE_PRODUCT_FAIL:
+            return {
+                loading: false,
+                error: action.payload,
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+        default:
+            return state;
+    }
+}
+
+export  { productReducer, singleProductReducer };

6eae4a0db2f177fa64b9b80ef45277cf77de4f20
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-18 09:19:47 +0530

    Map the data taking from home component

--- frontend/src/Components/ProductDispaly/ProductDisplay.jsx
+++ frontend/src/Components/ProductDispaly/ProductDisplay.jsx
@@ -1,29 +1,39 @@
 import React from 'react'
 import './ProductDispaly.css'
 import ProductCard from '../ProductCard/ProductCard'
+import Loader from '../Loader/Loder';
 
-const product = {
-    name:"Blue T-Shirt",
-    images:[{url:"https://i.ibb.co/DRST11n/1.webp"}],
-    price:200,
-    _id:"vishalyadav"
-}
+// const product = {
+//     name:"Blue T-Shirt",
+//     images:[{url:"https://i.ibb.co/DRST11n/1.webp"}],
+//     price:200,
+//     _id:"vishalyadav"
+// }
 
-const ProductDisplay = () => {
+const ProductDisplay = ({ loading, error, product }) => {
+  console.log(product)
   return (
+
     <div className='product-section' id='product-section'>
-        <div className="product-container">
-            <div className="heading">
-            <h2>Featured Products</h2>
-            </div>
-            <div className="content">
-                <ProductCard product={product}/>
-                <ProductCard product={product}/>
-                <ProductCard product={product}/>
-                <ProductCard product={product}/>
-                <ProductCard product={product}/>
-            </div>
+      <div className="product-container">
+        <div className="heading">
+          <h2>Featured Products</h2>
         </div>
+        {
+          loading
+            ? <Loader />
+            : <div className="content">
+              {
+                product && product.map((item) => {
+                  return (
+                    <ProductCard item={item} key={item._id} />
+                  )
+                })
+              }
+
+            </div>
+        }
+      </div>
     </div>
   )
 }

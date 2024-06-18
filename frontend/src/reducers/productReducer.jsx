e72f78297d56881d87deac0a14ada9abe458ceea
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-18 09:11:58 +0530

    Create the reducer for fetching the product from database using switch case statement according to their constant and the data store in state this function taking the state and action

---
+++ frontend/src/reducers/productReducer.jsx
@@ -0,0 +1,35 @@
+import {
+    ALL_PRODUCT_REQUEST,
+    ALL_PRODUCT_SUCEESS,
+    ALL_PRODUCT_FAIL,
+    CLEAR_ERRORS,
+} from '../constants/productConstants';
+const productReducer = (state = { products: [] }, action) => {
+    switch (action.type) {
+        case ALL_PRODUCT_REQUEST:
+            return {
+                loading: true,
+                product: [],
+            }
+        case ALL_PRODUCT_SUCEESS:
+            return {
+                loading: false,
+                product: action.payload,
+                // productsCount: action.payload.count,
+            }
+        case ALL_PRODUCT_FAIL:
+            return {
+                loading: false,
+                product: action.payload,
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+        default:
+            return state;
+    }
+}
+
+export default productReducer;

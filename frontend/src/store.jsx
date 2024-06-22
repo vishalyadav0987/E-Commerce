480c5f3bef266ea1cbfb7b0c72e769f8c97fae7a
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-22 17:30:41 +0530

    Combine the userReducer as user using combineReducer Method.

--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productReducer, singleProductReducer } from './reducers/productReducer';
+import { userReducer } from './reducers/userReducer';
 
 const reducer = combineReducers({
     products: productReducer,
-    singleProduct: singleProductReducer
+    singleProduct: singleProductReducer,
+    user: userReducer,
 });
 
 let initialState = {};

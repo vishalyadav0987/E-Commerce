a78677e1a64a769fbed321332c0ee057d3f68a40
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-22 17:33:44 +0530

    Add logic of login the user fetch the data from store using useSelector send the parameter and call the function on form submit

--- frontend/src/Pages/LoginSignUp/LoginSignUp.jsx
+++ frontend/src/Pages/LoginSignUp/LoginSignUp.jsx
@@ -1,9 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import './LoginSignUp.css';
-import axios from 'axios';
-import { toast } from 'react-hot-toast'
+import { toast } from 'react-hot-toast';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, clearError } from '../../actions/userAction';
+import { useAlert } from 'react-alert'
+import Loader from '../../Components/Loader/Loder';
+import { useNavigate } from 'react-router-dom'
+// import { login, register } from '../../actions/userAction';
 
 const LoginSignUp = () => {
+    const navigate = useNavigate();
+    const alert = useAlert();
+    const dispatch = useDispatch();
+
+    const { error, loading, isAuthenticate } = useSelector((state) => state.user)
+
     const [currState, setCurState] = useState("Login");
     const [data, setData] = useState({
         name: "",
@@ -11,120 +22,133 @@ const LoginSignUp = () => {
         password: "",
     });
 
-    const [avtar, setAvtar] = useState()
-    const [avtarPreview, setAvtarPreview] = useState("http://www.pngall.com/wp-content/uploads/5/Profile.png");
+    const [avatar, setAvatar] = useState(null);
+    const [avatarPreview, setAvatarPreview] = useState("http://www.pngall.com/wp-content/uploads/5/Profile.png");
 
     const onChangeHandler = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-        if (e.target.name === "avtar") {
-            const filereader = new FileReader();
-            filereader.onload = () => {
-                if (filereader.readyState === 2) {
-                    setAvtarPreview(filereader.result);
-                    setAvtar(filereader.result)
-                }
-            }
+
+        if (name === "avatar") {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+
+            reader.onloadend = () => {
+                setAvatarPreview(reader.result);
+                setAvatar(file);
+            };
+
+            reader.readAsDataURL(file);
+        } else {
+            setData({ ...data, [name]: value });
         }
-        else {
-            setData({ ...data, [name]: value })
+    };
+
+    const loginSubmit = (e) => {
+        e.preventDefault();
+        dispatch(login(data.email, data.password));
+    };
+
+    const registerSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('name', data.name);
+        formData.append('email', data.email);
+        formData.append('password', data.password);
+        formData.append('avatar', avatar);
+
+        // dispatch(register(formData));
+    };
+
+    useEffect(() => {
+        if (error) {
+            alert.error(error)
+            dispatch(clearError())
         }
-    }
+        if (isAuthenticate) {
+            navigate('/account');
+        }
+    }, [dispatch, error, alert])
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
-    //   const onSubmitHandler = async (e) => {
-    //     e.preventDefault();
-    //     let newUrl = url;
-    //     if(currState==="Login"){
-    //       newUrl+='/api/v1/user/login';
-    //     }
-    //     else{
-    //       newUrl+='/api/v1/user/register';
-    //     }
-    //     const response = await axios.post(newUrl,data);
-    //     if(response.data.success){
-    //       setToken(response.data.token);
-    //       localStorage.setItem("token",response.data.token);
-    //       toast.success(response.data.message);
-    //       setShowLogin(false);
-    //     }
-    //     else{
-    //       toast.error(response.data.message);
-    //     }
-    //   }
+
     return (
         <>
-            <div className="login-register-container">
-                {/* <form className="form-container" onSubmit={onSubmitHandler}> */}
-                <form className="form-container">
-                    <div className="login-text">
-                        <h2>{currState}</h2>
-                        {/* <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" /> */}
-                        <img src={""} alt="" />
-                    </div>
-                    <div className="form-controls">
-                        {currState === "Login" ? "" : <input
-                            onChange={onChangeHandler}
-                            type="text"
-                            placeholder='your name'
-                            name='name'
-                            required
-                            value={data.name}
-                        />}
-                        <input
-                            onChange={onChangeHandler}
-                            type="email"
-                            name='email'
-                            required
-                            placeholder='Enter email '
-                            value={data.email}
-                        />
-                        <input
-                            onChange={onChangeHandler}
-                            type="password"
-                            name='password'
-                            required
-                            placeholder='Password'
-                            value={data.password}
-                        />
-                    </div>
-                    {
-                        currState === "Login"
-                            ? <p className="forget-password-text">Forget Password ?</p>
-                            : <div class="header">
-                                <img
-                                    src={avtarPreview}
-                                    alt="" />
-                                <label htmlFor="file">
-                                    <input
-                                        type="file"
-                                        name="avtar"
-                                        id="file"
-                                        onChange={onChangeHandler}
-                                        value={avtar} />
-                                    <span>Upload Profile</span>
-                                </label>
+            {
+                loading
+                    ? <Loader />
+                    : <div className="login-register-container">
+                        <form className="form-container" onSubmit={currState === "Login" ? loginSubmit : registerSubmit}>
+                            <div className="login-text">
+                                <h2>{currState}</h2>
+                                <img src={""} alt="" />
                             </div>
-                    }
-                    <button type='submit'>{currState === "Register" ? "Create account" : "Login"}</button>
-                    <div className="terms-condition-text">
-                        <div className='terms-condition'>
-                            <input type="checkbox" required />
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, alias.</p>
-                        </div>
-                        {
-                            currState === "Login"
-                                ? <p>Create a new account? <span onClick={() => setCurState("Register")}>Click here</span></p>
-                                : <p>Already have an account? <span onClick={() => setCurState("Login")}>Click here</span></p>
-                        }
+                            <div className="form-controls">
+                                {currState === "Register" && (
+                                    <input
+                                        onChange={onChangeHandler}
+                                        type="text"
+                                        placeholder='Your Name'
+                                        name='name'
+                                        required
+                                        value={data.name}
+                                    />
+                                )}
+                                <input
+                                    onChange={onChangeHandler}
+                                    type="email"
+                                    name='email'
+                                    required
+                                    placeholder='Enter Email'
+                                    value={data.email}
+                                />
+                                <input
+                                    onChange={onChangeHandler}
+                                    type="password"
+                                    name='password'
+                                    required
+                                    placeholder='Password'
+                                    value={data.password}
+                                />
+                            </div>
+                            {currState === "Register" && (
+                                <div className="header">
+                                    <img src={avatarPreview} alt="Avatar Preview" />
+                                    <label htmlFor="file">
+                                        <input
+                                            type="file"
+                                            name="avatar"
+                                            id="file"
+                                            onChange={onChangeHandler}
+                                            accept="image/*"
+                                        />
+                                        <span>Upload Profile</span>
+                                    </label>
+                                </div>
+                            )}
+                            {
+                                currState === "Login"
+                                    ? <p className="forget-password-text">Forget Password</p>
+                                    : ""
+                            }
+                            <button type='submit'>{currState === "Register" ? "Create Account" : "Login"}</button>
+                            <div className="terms-condition-text">
+                                <div className='terms-condition'>
+
+                                </div>
+                                {currState === "Login" ? (
+                                    <p>Create a new account? <span onClick={() => setCurState("Register")}>Click here</span></p>
+                                ) : (
+                                    <p>Already have an account? <span onClick={() => setCurState("Login")}>Click here</span></p>
+                                )}
+                            </div>
+                        </form>
                     </div>
-
-                </form>
-            </div>
+            }
         </>
-    )
-}
+    );
+};
 
-export default LoginSignUp
+export default LoginSignUp;

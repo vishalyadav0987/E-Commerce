1487cfe805474b26bfb1af6e67192cb2308b0d3b
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-27 16:20:12 +0530

    Create register reducer for user set the all  the of user in a redux store.

--- frontend/src/reducers/userReducer.jsx
+++ frontend/src/reducers/userReducer.jsx
@@ -2,17 +2,22 @@ import {
     LOGIN_REQUEST,
     LOGIN_SUCEESS,
     LOGIN_FAIL,
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCEESS,
+    REGISTER_USER_FAIL,
     CLEAR_ERRORS,
 } from '../constants/userConstants';
 
 const userReducer = (state = { user: {} }, action) => {
     switch (action.type) {
         case LOGIN_REQUEST:
+        case REGISTER_USER_REQUEST:
             return {
                 loading: true,
                 isAuthenticate: false,
             }
         case LOGIN_SUCEESS:
+        case REGISTER_USER_SUCEESS:
             return {
                 ...state,
                 loading: false,
@@ -20,6 +25,7 @@ const userReducer = (state = { user: {} }, action) => {
                 user: action.payload,
             }
         case LOGIN_FAIL:
+        case REGISTER_USER_FAIL:
             return {
                 ...state,
                 loading: false,

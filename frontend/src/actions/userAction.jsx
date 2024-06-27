d11911a7ac576c7b434a891836da6f1871972b6a
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-27 16:22:36 +0530

    Create register action function take form data as parameter from the user and store data in to database using axios library by post method using this route ["{/api/v1/user/register}"].

--- frontend/src/actions/userAction.jsx
+++ frontend/src/actions/userAction.jsx
@@ -2,6 +2,9 @@ import {
     LOGIN_REQUEST,
     LOGIN_SUCEESS,
     LOGIN_FAIL,
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCEESS,
+    REGISTER_USER_FAIL,
     CLEAR_ERRORS,
 } from '../constants/userConstants';
 import axios from 'axios'
@@ -30,6 +33,30 @@ const login = (email, password) => async (dispatch) => {
         });
     }
 }
+const register = (userData) => async (dispatch) => {
+    try {
+        dispatch({ type: REGISTER_USER_REQUEST });
+
+        const config = { headers: { "Content-Type": "multipart/form-data" } };
+        const response = await axios.post(
+            `/api/v1/user/register`,
+            userData, // data
+            config,
+        );
+
+        dispatch({
+            type: REGISTER_USER_SUCEESS,
+            payload: response.data.user,
+        })
+    } catch (error) {
+        dispatch({
+            type: REGISTER_USER_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+        });
+    }
+}
 
 
 
@@ -37,4 +64,4 @@ const login = (email, password) => async (dispatch) => {
 const clearError = () => async (dispatch) => {
     dispatch({ type: CLEAR_ERRORS });
 }
-export { login, clearError };
+export { login, clearError, register };

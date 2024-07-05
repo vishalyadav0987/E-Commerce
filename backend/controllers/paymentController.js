d85ac6d4e71a0c0549590aa8bab10279db4fd100
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-05 19:26:03 +0530

    Create the processPayment using stripe package and also create sendStripeApiKey function pass apiKey in response for frontend.

---
+++ backend/controllers/paymentController.js
@@ -0,0 +1,28 @@
+const stripe = require('stripe')(process.env.STRIPE_SECERET_KEY);
+require('dotenv').config()
+
+const processPayment = async (req, res) => {
+    try {
+        const myPayment = await stripe.paymentIntents.create({
+            amount: req.body.amount,
+            currency: "inr",
+            metadata: {
+                company: "E-Commerce"
+            },
+
+        });
+
+        res.json({ success: true, client_secret: myPayment.client_secret })
+    } catch (error) {
+        console.log("Erroror in processPayment");
+        res.json({ success: false, message: error.message })
+    }
+}
+
+const sendStripeApiKey = async(req,res)=>{
+    res.json({ success: true, stripeApiKey:process.env.STRIPE_API_KEY })
+}
+module.exports = {
+    processPayment,
+    sendStripeApiKey
+}

e405ecaf41bdc9d567a4321f8083062bcbb4a024
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-06 22:07:40 +0530

    Change the if the user is Authenticate and check then load the page.

--- frontend/src/Utils/ProtectedRoute.jsx
+++ frontend/src/Utils/ProtectedRoute.jsx
@@ -1,47 +1,72 @@
-// import React from 'react'
+// // import React from 'react'
+// // import { useSelector } from 'react-redux';
+// // import { Navigate, Route } from 'react-router-dom'
+
+// // const ProtectedRoute = ({ element: Element, ...rest }) => {
+// //     const { loading, user, isAuthenticate } = useSelector(state => state.user);
+
+// //     return (
+// //         <>
+// //             {
+// //                 !loading && (
+// //                     <Route
+// //                         {...rest}
+// //                         element={
+// //                             isAuthenticate ? <Element /> : <Navigate to='/login' />
+// //                         }
+// //                     />
+// //                 )
+// //             }
+// //         </>
+// //     )
+// // }
+
+// // export default ProtectedRoute
+
+// import React from 'react';
 // import { useSelector } from 'react-redux';
-// import { Navigate, Route } from 'react-router-dom'
+// import { Navigate, Outlet } from 'react-router-dom';
 
-// const ProtectedRoute = ({ element: Element, ...rest }) => {
-//     const { loading, user, isAuthenticate } = useSelector(state => state.user);
+// const ProtectedRoute = () => {
+//     const { loading, isAuthenticate } = useSelector(state => state.user);
+//     // Create the protect route if the user is not authenticate user can't get profile page ornot acces 
+//     // outelet as elemt or componet with props it new tectnique to do or go to loginpage automatically
+//     // and also get rid from {!isAuthenticate && <Component/>}
 
-//     return (
-//         <>
-//             {
-//                 !loading && (
-//                     <Route
-//                         {...rest}
-//                         element={
-//                             isAuthenticate ? <Element /> : <Navigate to='/login' />
-//                         }
-//                     />
-//                 )
-//             }
-//         </>
-//     )
-// }
+//     //Create a protected route so that if the user is not authenticated, they cannot access the profile page or any other protected components. Use the Outlet component to render the nested routes. If the user is not authenticated, they should be automatically redirected to the login page. This technique replaces the need for conditionally rendering components based on authentication status, such as {!isAuthenticate && <Component />}.
 
-// export default ProtectedRoute
+
+//     if (loading) {
+//         return <div>Loading...</div>; // Or any other loading indicator you prefer
+//     }
+
+//     return isAuthenticate  ? <Outlet /> : <Navigate to='/login' />;
+// };
+
+// export default ProtectedRoute;
+
 
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
+import Loader from '../Components/Loader/Loder'
 
 const ProtectedRoute = () => {
-    const { loading, isAuthenticate } = useSelector(state => state.user);
-    // Create the protect route if the user is not authenticate user can't get profile page ornot acces 
-    // outelet as elemt or componet with props it new tectnique to do or go to loginpage automatically
-    // and also get rid from {!isAuthenticate && <Component/>}
-
-    //Create a protected route so that if the user is not authenticated, they cannot access the profile page or any other protected components. Use the `Outlet` component to render the nested routes. If the user is not authenticated, they should be automatically redirected to the login page. This technique replaces the need for conditionally rendering components based on authentication status, such as `{!isAuthenticate && <Component />}`.
-
+    const { loading, isAuthenticate, user } = useSelector((state) => state.user);
 
     if (loading) {
-        return <div>Loading...</div>; // Or any other loading indicator you prefer
+        return <Loader/>; // Or any other loading indicator you prefer
     }
 
-    return isAuthenticate ? <Outlet /> : <Navigate to='/login' />;
+    if (isAuthenticate === false) {
+        return <Navigate to="/login" />;
+    }
+
+    // if (isAdmin && user.role !== 'admin') {
+    //     return <Navigate to="/login" />;
+    // }
+
+    return <Outlet />;
 };
 
 export default ProtectedRoute;
-

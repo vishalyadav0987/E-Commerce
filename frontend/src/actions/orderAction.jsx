d6c98ec7e17f544d597020a4f2ccd5c906c58b03
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-06 22:10:19 +0530

    Create getMyOrder action or fuction fetch the data from db using axios library by get using [{/api/v1/order/me/orders}] these route and send order data in payload.

--- frontend/src/actions/orderAction.jsx
+++ frontend/src/actions/orderAction.jsx
@@ -2,6 +2,9 @@ import {
     CREATE_ORDER_REQUEST,
     CREATE_ORDER_SUCCESS,
     CREATE_ORDER_FAIL,
+    MY_ORDER_REQUEST,
+    MY_ORDER_SUCCESS,
+    MY_ORDER_FAIL,
     CLEAR_ERRORS
 } from '../constants/orderConstants';
 import axios from 'axios'
@@ -29,9 +32,29 @@ const createOrder = (orderDetails) => async (dispatch) => {
 }
 
 
+//Fetch MY ORDER
+const getMyOrder = () => async (dispatch) => {
+    try {
+        dispatch({ type: MY_ORDER_REQUEST })
+        const response = await axios.get('/api/v1/order/me/orders');
+        dispatch({
+            type: MY_ORDER_SUCCESS,
+            payload: response.data.data
+        })
+    } catch (error) {
+        dispatch({
+            type: MY_ORDER_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+        });
+    }
+}
+
+
 // clearing Error
 const clearError = () => async (dispatch) => {
     dispatch({ type: CLEAR_ERRORS });
 }
 
-export { createOrder,clearError }
+export { createOrder, getMyOrder,clearError }

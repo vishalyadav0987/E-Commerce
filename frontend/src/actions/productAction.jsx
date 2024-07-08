98567907f4d60ecbeb2b1bc4d9e75504330d28d4
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-08 19:43:42 +0530

    Fetch data using axios libray using get method send it to payload.

--- frontend/src/actions/productAction.jsx
+++ frontend/src/actions/productAction.jsx
@@ -10,6 +10,9 @@ import {
     NEW_REVIEW_REQUEST,
     NEW_REVIEW_SUCEESS,
     NEW_REVIEW_FAIL,
+    ADMIN_ALL_PRODUCT_REQUEST,
+    ADMIN_ALL_PRODUCT_SUCEESS,
+    ADMIN_ALL_PRODUCT_FAIL,
 } from '../constants/productConstants';
 
 const getAllProducts = (keyword = "", currentPage = 1, price = [200, 10000], category, ratings = 0) => async (dispatch) => {
@@ -31,6 +34,23 @@ const getAllProducts = (keyword = "", currentPage = 1, price = [200, 10000], cat
     } catch (error) {
         dispatch({
             type: ALL_PRODUCT_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+// FTCH ALL PRODUCT LIST FOR ADMIN
+const productForAdminPanel = () => async (dispatch) => {
+    try {
+        dispatch({ type: ADMIN_ALL_PRODUCT_REQUEST });
+        const response = await axios.get('/api/v1/product/admin/products');
+        dispatch({
+            type: ADMIN_ALL_PRODUCT_SUCEESS,
+            payload: response.data.data,
+        })
+    } catch (error) {
+        dispatch({
+            type: ADMIN_ALL_PRODUCT_FAIL,
             payload: error.response.data.message
         })
     }
@@ -83,4 +103,4 @@ const clearError = () => async (dispatch) => {
 }
 
 
-export { getAllProducts, clearError, getSingleProducts,newReview }
+export { getAllProducts, clearError, getSingleProducts, newReview, productForAdminPanel }

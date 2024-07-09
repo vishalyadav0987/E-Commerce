34a04560fc65734792ba80fc33d8a44fcd43ed85
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-09 19:26:38 +0530

    Change admin route.

--- frontend/src/Components/UserNavOption/UserNavOption.jsx
+++ frontend/src/Components/UserNavOption/UserNavOption.jsx
@@ -21,7 +21,7 @@ const UserNavOption = ({ user }) => {
     const { cartItems } = useSelector(state => state.cart)
 
     const dashboard = () => {
-        navigate('/dashboard');
+        navigate('/admin/dashboard');
     }
     const orders = () => {
         navigate('/orders');

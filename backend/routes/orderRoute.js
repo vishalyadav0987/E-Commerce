ca9f03e967632d3f0b62ee9f9293310f29724a9e
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-12 19:24:00 +0530

    For Update route change method post to put method.

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -14,7 +14,7 @@ router.route('/orders/place').post(protecteRoute, newOrder);
 router.route('/orders/:id').get(protecteRoute, getSingleOrderDetail);
 router.route('/me/orders').get(protecteRoute, myOrder);
 router.route('/orders').get(protecteRoute, authorizeRole("admin"), getAllOrders);
-router.route('/orders/update/status/:id').post(protecteRoute, authorizeRole("admin"), updateOrderStatus);
+router.route('/orders/update/status/:id').put(protecteRoute, authorizeRole("admin"), updateOrderStatus);
 router.route('/orders/delete/:id').delete(protecteRoute, authorizeRole("admin"), deleteOrder);
 
 module.exports = router;

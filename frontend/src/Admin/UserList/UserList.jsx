290124740bd51c2c3fe8f0d1b6833100baeaf47c
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-12 20:17:13 +0530

    Implemented UserList Component
    - Developed a user management component `UserList` for an admin dashboard to display all users with columns for user ID, name, email, and role.
    - Utilized Material-UI's DataGrid for a structured and responsive table layout, enhancing user experience with pagination and selection features.
    - Integrated actions for editing and deleting users using icons (EditIcon, DeleteIcon) and buttons, providing seamless interaction with user data.
    - Implemented functionality to delete users with confirmation alerts and success messages using react-alert for user feedback.
    - Incorporated navigation links to individual user details for editing, maintaining a coherent navigation flow within the admin panel.
    - Implemented error handling for API requests, displaying error alerts and clearing errors using Redux actions to ensure smooth operation.
    - Included styling enhancements using CSS modules to maintain consistent design across the application's user management section.

---
+++ frontend/src/Admin/UserList/UserList.jsx
@@ -0,0 +1,128 @@
+import React, { useEffect } from 'react';
+import { DataGrid } from '@material-ui/data-grid';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAlert } from 'react-alert';
+import { useDispatch, useSelector } from 'react-redux';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { Button } from '@material-ui/core';
+import SideBar from '../SideBar/SideBar';
+import { clearError, deleteUser, getAllUsers } from '../../actions/userAction';
+import './UserList.css'
+import { DELETE_USER_RESET } from '../../constants/userConstants';
+
+const UserList = () => {
+  const navigate = useNavigate();
+  const alert = useAlert();
+  const dispatch = useDispatch();
+  const { users, error } = useSelector(state => state.allUser);
+  const { isDeleted, message: deleteMessage, error: DeleteError } = useSelector(state => state.updateAndDeleteUser);
+
+  const deleteUserHandler = (id) => {
+    dispatch(deleteUser(id))
+  }
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearError());
+    }
+    if (DeleteError) {
+      alert.error(DeleteError);
+      dispatch(clearError());
+    }
+    if (isDeleted) {
+      alert.success(deleteMessage);
+      navigate("/admin/users");
+      dispatch({ type: DELETE_USER_RESET });
+    }
+    dispatch(getAllUsers());
+  }, [dispatch, error, DeleteError, navigate, isDeleted]);
+
+
+
+
+  const columns = [
+    {
+      field: 'id', headerName: 'User ID', minWidth: 100,
+      flex: 0.5
+    },
+    {
+      field: 'name', headerName: 'Name', minWidth: 150,
+      flex: 0.4,
+    },
+    {
+      field: 'email', headerName: 'Email ID', minWidth: 250,
+      flex: 0.8
+    },
+    {
+      field: 'role', headerName: 'Role', minWidth: 100,
+      flex: 0.3,
+      cellClassName: (params) => {
+        return params.getValue(params.id, "role") === "admin"
+          ? "greenColor"
+          : "redColor"
+      }
+    },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      minWidth: 100,
+      flex: 0.5,
+      type: 'number',
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/admin/user/${params.getValue(params.id, 'id')}`}>
+              <EditIcon />
+            </Link>
+            <Button onClick={() => {
+              deleteUserHandler(params.getValue(params.id, 'id'))
+            }}><DeleteIcon /></Button>
+          </>
+        );
+      }
+    }
+  ];
+
+  const rows = [];
+
+  users && users.forEach((user) => {
+    rows.push({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    })
+  });
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  })
+  return (
+    <div>
+      <div className="dashboard dashboard-ok dashboard-userList">
+        <SideBar />
+        <div className="productListContainer">
+          <h1 id="productListHeading">ALL USERS</h1>
+          {/* { */}
+          {/* loading */}
+          {/* ? <Loader /> */}
+          {/* : */}
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={10}
+            disableSelectionOnClick
+            className="productListTable"
+            autoHeight
+          />
+          {/* } */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UserList

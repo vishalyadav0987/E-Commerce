3dd264896332ed2b1022e44eff295ec017875a12
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-12 19:54:18 +0530

    Change id to productId because of backend product saved id as productId.

--- frontend/src/reducers/cartReducre.jsx
+++ frontend/src/reducers/cartReducre.jsx
@@ -1,16 +1,17 @@
 import { ADD_TO_CART, REMOVE_ITEM_FROM_CART, SAVE_SHIPPING_INFO } from "../constants/cartConstants";
 
-const cartReducre = (state = { cartItems: [] }, action) => {
+const cartReducre = (state = { cartItems: [], shippingInfo: {} }, action) => {
     switch (action.type) {
         case ADD_TO_CART:
             const item = action.payload;
 
-            const isItemExsit = state.cartItems.find((product) => product.id === item.id)
+            const isItemExsit = state.cartItems.find((product) => product.productId === item.productId)
+            console.log(isItemExsit, item.productId)
             if (isItemExsit) {
                 return {
                     ...state,
                     cartItems: state.cartItems.map((product) =>
-                        product.id === isItemExsit.id ? item : product)
+                        product.productId === isItemExsit.productId ? item : product)
                 }
             }
             else {
@@ -23,7 +24,7 @@ const cartReducre = (state = { cartItems: [] }, action) => {
             return {
                 ...state,
                 cartItems: state.cartItems.filter((product) =>
-                    product.id !== action.payload) // reduer me hum payload me id bhej raeh hai
+                    product.productId !== action.payload) // reduer me hum payload me id bhej raeh hai
             }
         case SAVE_SHIPPING_INFO:
             return {

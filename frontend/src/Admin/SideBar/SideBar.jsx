9b9ecadbf0a294fc018140980dcbd722b971e392
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-12 20:09:15 +0530

    Add Sidebar Component for Admin Panel Navigation
    - Implemented Sidebar component to provide navigation links for an ecommerce admin panel.
    - Integrated Material-UI icons for dashboard, products, orders, users, and reviews navigation.
    - Utilized conditional rendering to display a search review option based on the current route.
    - Styled components for improved visual presentation and user interaction.
    - Linked navigation items to corresponding routes within the application for seamless user experience.

--- frontend/src/Admin/SideBar/SideBar.jsx
+++ frontend/src/Admin/SideBar/SideBar.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideBar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { TreeView, TreeItem } from "@material-ui/lab";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import PostAddIcon from "@material-ui/icons/PostAdd";
@@ -10,13 +10,15 @@ import ListAltIcon from "@material-ui/icons/ListAlt";
 import DashboardIcon from "@material-ui/icons/Dashboard";
 import PeopleIcon from "@material-ui/icons/People";
 import RateReviewIcon from "@material-ui/icons/RateReview";
+import { MdReviews } from "react-icons/md";
 
-const SideBar = () => {
+const SideBar = ({searchReviewOpen,clickHandler}) => {
+  const location = useLocation()
   return (
     <div className="sidebar">
       <Link to="/">
         <img src={"https://clipground.com/images/ecommerce-logo-png-19.png"}
-         alt="Ecommerce" style={{width:"180px",padding: "48px 16px 32px 16px"}}/>
+          alt="Ecommerce" style={{ width: "180px", padding: "48px 16px 32px 16px" }} />
       </Link>
       <Link to="/admin/dashboard">
         <p>
@@ -56,6 +58,30 @@ const SideBar = () => {
           Reviews
         </p>
       </Link>
+      {
+        location.pathname === '/admin/reviews'
+        ? <div onClick={clickHandler}
+        style={{
+          textDecoration: "none",
+          font: "200 1.5rem Roboto",
+          padding: "2rem",
+          transition: "all 0.5s",
+          color: "white",
+          cursor:"pointer"
+        }}>
+        <p
+          style={{
+            display: "flex",
+            alignItems: "center"
+          }}>
+          <MdReviews  style={{
+            marginRight:"5px"
+          }}/>
+          Search Review by Id
+        </p>
+      </div>
+      :""
+      }
     </div>
   );
 };
